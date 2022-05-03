048d32b2f0802570e24be5378ef75f6d6f74c95b
Anushka Vidanage <vidanage.anushka@gmail.com>
2022-05-03 12:00:26 +1000

    changes to pubspec

--- lib/solid_encrypt.dart
+++ lib/solid_encrypt.dart
@@ -5,12 +5,6 @@ import 'dart:async';
 import 'dart:convert';
 
 /// Package imports:
-// import 'package:http/http.dart' as http;
-// import 'package:solid_auth/src/openid/openid_client.dart';
-// import 'package:solid_auth/src/jwt/dart_jsonwebtoken.dart';
-// import 'package:uuid/uuid.dart';
-// import 'package:url_launcher/url_launcher.dart';
-// import 'package:fast_rsa/fast_rsa.dart';
 import 'package:solid_auth/solid_auth.dart';
 import 'package:encrypt/encrypt.dart';
 import 'package:crypto/crypto.dart';
@@ -18,12 +12,9 @@ import 'package:localstorage/localstorage.dart';
 
 /// Project imports:
 import 'package:solid_encrypt/src/rdf_data.dart';
-//import 'package:solid_auth/platform_info.dart';
-//import 'package:solid_auth/src/openid/openid_client_io.dart' as oidc_mobile;
-//import 'package:solid_auth/src/auth_manager/auth_manager_abstract.dart';
 
 part 'enc_client.dart';
 part 'key_hash.dart';
 
-///Local storage instance
+/// Local storage instance
 final LocalStorage APP_STORAGE = new LocalStorage('app_storage');
--- test/solid_encrypt_test.dart
+++ test/solid_encrypt_test.dart
@@ -2,6 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:solid_encrypt/solid_encrypt.dart';
 
 void main() {
-  test('testing encrypt functions', () {
+  test('testing encryption functions', () {
   });
 }
